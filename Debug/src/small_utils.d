fc54e0f1f4cb6ae254dc5da98704a9350bcd3c15
Sven Steudte <sven.steudte@gmail.com>
2015-05-31 01:15:21 +0000

    Deleted binaries

--- Debug/src/small_utils.d
+++
@@ -1,4 +0,0 @@
-src/small_utils.o src/small_utils.d: ../src/small_utils.c \
- ../src/small_utils.h
-
-../src/small_utils.h:
--- Release/src/small_printf_support.d
+++
@@ -1,6 +0,0 @@
-src/small_printf_support.o src/small_printf_support.d: \
- ../src/small_printf_support.c ../src/small_printf.h ../src/small_utils.h
-
-../src/small_printf.h:
-
-../src/small_utils.h:
