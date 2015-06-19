ccaf10783e52ecb76778f74e37569cbcffab0969
Sven Steudte <sven.steudte@gmail.com>
2015-06-19 22:39:53 +0200

    Added local files

--- src/config.h
+++ src/config.h
@@ -44,8 +44,8 @@
 // - Home:       0
 // - IGate:      5
 //
-#define S_CALLSIGN				"DK0TU"
-#define S_CALLSIGN_ID			13
+#define S_CALLSIGN				"DL7AD"
+#define S_CALLSIGN_ID			11
 
 // Destination callsign: APRS (with SSID=0) is usually okay.
 #define D_CALLSIGN				"APECAN"  // APExxx = Telemetry devices (that's what Pecan actually is)
@@ -365,8 +365,8 @@
 	#define GPS_PIO_EN			PIO1_8
 	#define GPS_PIN_EN			(1 << 8)
 
-	//#define OSC_FREQ(u)			((u*623/1024)+19997384)	// Oscillator frequency 20MHz !R10=3k3k!
-	#define OSC_FREQ(u)			((u*3024/1024)+26990164)	// Oscillator frequency 27MHz !R10=10k!
+	#define OSC_FREQ(u)			((u*623/1024)+19997384)	// Oscillator frequency 20MHz !R10=3k3k!
+	//#define OSC_FREQ(u)			((u*3024/1024)+26990164)	// Oscillator frequency 27MHz !R10=10k!
 #else
 	#error No/incorrect target selected
 #endif
