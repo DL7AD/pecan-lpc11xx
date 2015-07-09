00e082a1d7882a6ee0c3ffed600ccc0f07d0d757
unknown <sven.steudte@gmail.com>
2015-07-10 00:29:05 +0200

    Corrected NMEA coordinate to Float conversion error

--- src/gps.c
+++ src/gps.c
@@ -292,7 +292,7 @@ void parse_lat(const char *token)
 		mins[0] = token[2];
 		mins[1] = token[3];
 		mins[2] = '\0';
-		newFix.latitude = atoi(degs) + atoi(mins) / (double)60 + atoi(token + 5) / (double)6000000;
+		newFix.latitude = atoi(degs) + atoi(mins) / (double)60 + atoi(token + 5) / (double)600000;
 	}
 }
 
@@ -315,7 +315,7 @@ void parse_lon(const char *token)
 		mins[0] = token[3];
 		mins[1] = token[4];
 		mins[2] = '\0';
-		newFix.longitude = atoi(degs) + atoi(mins) / (double)60 + atoi(token + 6) / (double)6000000;
+		newFix.longitude = atoi(degs) + atoi(mins) / (double)60 + atoi(token + 6) / (double)600000;
 	}
 }
 
